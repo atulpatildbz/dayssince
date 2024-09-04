69f101748cdfcc9674eefa3af44ac573ef886b0a
Atul Patil <atulpatildbz@gmail.com>
2024-09-04 10:48:41 +0530

    remove bun + convert to pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,37 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dayssince/",
-  plugins: [react()],
-});
+  base: '/dayssince/',
+  plugins: [react(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'dayssince',
+      short_name: 'dayssince',
+      description: 'Track the number of days since an event occured',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
