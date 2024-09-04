5550353389905f44efe9da20d0fd9c120af18dd0
Atul Patil <atulpatildbz@gmail.com>
2024-09-04 11:15:14 +0530

    test after update

--- src/App.tsx
+++ src/App.tsx
@@ -264,7 +264,7 @@ function App() {
         <div className="relative flex-grow mr-4">
           <input
             type="text"
-            placeholder="Search events..."
+            placeholder="Search"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             className="w-full pl-10 pr-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
