a169800a8630431c30e502859a2185f106e90218
Atul Patil <atulpatildbz@gmail.com>
2024-09-04 09:14:40 +0530

    Revert "revert pwa"
    This reverts commit 720df04dfdcca337684515c41d9f923cf1095025.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/dayssince/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.ts',
+      registerType: "autoUpdate",
+      manifest: {
+        name: 'Days Since',
+        short_name: 'DaysSince',
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      injectManifest: {
+        injectionPoint: undefined
+      }
+    }),
+  ],
 });
